31715f45ca45ed2841492dd7069fa475b8996eb6
Philip Wilk <wiryfuture@gmail.com>
2022-04-13 16:25:33 +0100

    function defs

---
+++ IncomingVoiceNormaliser.plugin.js
@@ -0,0 +1,44 @@
+/**
+ * @name IncomingVoiceNormaliser
+ * @version 0.0.1
+ * @description Normalise the volume of other people in voice channels so perceieved volume is the same.
+ * @author wiryfuture
+ * @authorLink https://github.com/wiryfuture
+ */
+
+let IncomingVoiceNormaliser = () => {
+
+}
+
+// On user click of settings button for plugin
+let getSettingsPanel = () => {
+
+}
+
+// On pllugin load (regardless of enable/disable state)
+let load = () => {
+
+}
+
+// On plugin enable/load
+let start = () => {
+
+}
+
+// On plugin disable
+let stop = () => {
+
+}
+
+/** On mutations of document
+ * "What are mutations/observers?" => https://developer.mozilla.org/en-US/docs/Web/API/Mutation/Observer
+ * changes: MutationRecord
+*/
+let observer = (changes) => {
+
+}
+
+let onSwitch = () => {
+    
+}
+
